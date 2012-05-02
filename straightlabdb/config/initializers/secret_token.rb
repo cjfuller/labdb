973684481ce642f6b7019de7bd18d1eaca2a61d9
Colin Fuller <cjfuller@gmail.com>
2012-05-01 23:25:06 -0700

    initial creation of project

---
+++ straightlabdb/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Straightlabdb::Application.config.secret_token = 'b0d023a4a9fb9bd95a0908a71cec58d818ea04eb3c648391b6d282e12d46143cba29995ac75a97169655c9cfcd810c17daa3ef6ed362485da47b0483e1757f2a'
