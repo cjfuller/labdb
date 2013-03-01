8d101c070083e245c8ee62aef5e56eef17eb6829
Colin J. Fuller <cjfuller@gmail.com>
2013-02-28 16:15:11 -0800

    added use of ca certificates

--- straightlabdb/config/initializers/omniauth.rb
+++ straightlabdb/config/initializers/omniauth.rb
@@ -1,5 +1,8 @@
 require 'omniauth-openid'
 require 'openid/store/filesystem'
+require 'openid/fetchers'
+
+OpenID.fetcher.ca_file = "#{Rails.root}/config/ca-certificates.crt"
 
 Rails.application.config.middleware.use Rack::Session::Cookie
 
