ea693bb4fa8ebf4d9c52e7d40d91fb59ab2a8583
Colin J. Fuller <cjfuller@gmail.com>
2014-01-27 17:28:53 -0800

    add helptext for plasmid map

--- lib/helptext.rb
+++ lib/helptext.rb
@@ -17,14 +17,15 @@
 
 module Helptext
 
-  HELPTEXT_LOCS = { description_formatting: "lib/assets/description_helptext.md" }
+  HELPTEXT_LOCS = { description_formatting: "lib/assets/description_helptext.md", plasmid_map: "lib/assets/plasmid_map.md" }
 
   def self.get(name)
-
-    puts HELPTEXT_LOCS[name]
-
-    HELPTEXT_LOCS[name] and Labdb::Application::MARKDOWN.render(File.read(HELPTEXT_LOCS[name])).html_safe
-
+    if HELPTEXT_LOCS[name] then
+      helptext = Labdb::Application::MARKDOWN.render(File.read(HELPTEXT_LOCS[name])).html_safe
+      "<div class='helptext-container'>#{helptext}</div>".html_safe
+    else
+      nil
+    end
   end
 
 end
