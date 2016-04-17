2dff4dfb7ddc4c145a2799cbc8886d48dda807e1
Colin Fuller <colin@khanacademy.org>
2016-04-17 11:56:31 -0700

    Whitespace cleanup

--- app/controllers/searching.rb
+++ app/controllers/searching.rb
@@ -17,151 +17,118 @@
 
 module Searching
 
-	REGEX_DETECTION_REGEX = /\A\/.*\/(i?)\Z/
+  REGEX_DETECTION_REGEX = /\A\/.*\/(i?)\Z/
 
-	def delete_old_searches
+  def delete_old_searches
+    all_searches = Search.all
 
-		all_searches = Search.all
+    unless all_searches.nil? then
+      all_searches.each { |s| s.destroy if s.expired? }
+    end
 
-		unless all_searches.nil? then
-			all_searches.each { |s| s.destroy if s.expired? }
-		end
+    searches = Search.where(user_id: curr_user_id)
 
-		searches = Search.where(user_id: curr_user_id)
-
-		unless searches.nil? then
-			searches.each { |s| s.destroy }
-		end
-
-	end
+    unless searches.nil? then
+      searches.each { |s| s.destroy }
+    end
+  end
 
 
-	def generate_search_object(search_params, result)
-
-		delete_old_searches
-
-		sec_per_day = 60*60*24
-
-		exp_time = Time.now + 1.5*sec_per_day
-
-		res_hash = result.inject({}) { |h, e| h[e.id]= e.number_field; h }
-
+  def generate_search_object(search_params, result)
+    delete_old_searches
+    sec_per_day = 60*60*24
+    exp_time = Time.now + 1.5*sec_per_day
+    res_hash = result.inject({}) { |h, e| h[e.id]= e.number_field; h }
     params = ActionController::Parameters.new(expires: exp_time,
                                               result: Psych.dump(res_hash),
                                               searchparams: search_params,
                                               user_id: curr_user_id)
+    s = Search.new(params.permit(:expires, :result, :searchparams, :user_id))
+    s.save
+  end
 
-		s = Search.new(params.permit(:expires, :result, :searchparams, :user_id))
+  def valid_search_requested?
+    return false unless params.has_key?(:search_id)
+    current_search = find_current_search
+    current_search and current_search.id == params[:search_id].to_i
+  end
 
-		s.save
+  def find_current_search
+    s = Search.find_by_user_id(curr_user_id)
+    return nil if s.nil? or s.expired?
 
-	end
+    begin
+      s.loaded_result = Psych.load(s.result) unless s.loaded_result
+    rescue ArgumentError => e
+      logger.warn("Exception encountered while loading stored search result.  Likely a development mode lazy-loading issue: #{e.message}")
+    end
 
-	def valid_search_requested?
+    s
+  end
 
-		return false unless params.has_key?(:search_id)
+  def glob_style_search_to_regex(search_params, key)
+    search_params[key].gsub!("*", ".*")
+    #add start and end of string matchers to avoid, e.g., matching all plasmids with a 1 when searching for #1
+    search_params[key]= '\A' + search_params[key] + '\Z'
+    Regexp.new(search_params[key], true) #always case insensitive since there's no way to specify
+  end
 
-		current_search = find_current_search
+  def parse_search_regex(search_param)
+    matched = REGEX_DETECTION_REGEX.match(search_param)
+    end_of_regex_offset = 1
+    case_insensitive = (matched[1].length > 0)
 
-		current_search and current_search.id == params[:search_id].to_i
+    if case_insensitive then
+      end_of_regex_offset = 2
+    end
 
-	end
+    Regexp.new(search_param[1...(search_param.length-end_of_regex_offset)], case_insensitive)
+  end
 
-	def find_current_search
+  def generate_regex_conditions(search_params)
+    regex_conditions = {}
+    
+    search_params.each_key do |k|
+      if search_params[k] and search_params[k] != "" and k != "verified" then #TODO: is there a better way to deal with the verified field?
+        matched = REGEX_DETECTION_REGEX.match(search_params[k])
 
-		s = Search.find_by_user_id(curr_user_id)
+        if matched then
+          regex_conditions[k] = parse_search_regex(search_params[k])
+        else
+          regex_conditions[k] = glob_style_search_to_regex(search_params, k)
+        end
+      end
+    end
 
-		return nil if s.nil? or s.expired?
+    regex_conditions
+  end
+  
+  def process_search_query(search_params, search_class)
+    preprocessed_conditions = preprocess_search_query(search_params)
+    regex_conditions = generate_regex_conditions(search_params)
 
-		begin
-			s.loaded_result = Psych.load(s.result) unless s.loaded_result
-		rescue ArgumentError => e
-			logger.warn("Exception encountered while loading stored search result.  Likely a development mode lazy-loading issue: #{e.message}")
-		end
+    preprocessed_conditions.each do |k,v|
+      regex_conditions[k] = Regexp.new(v)
+    end
 
-		s
+    preliminary_list = search_class.order(index_order)
 
-	end
-
-	def glob_style_search_to_regex(search_params, key)
-
-		search_params[key].gsub!("*", ".*")
-		#add start and end of string matchers to avoid, e.g., matching all plasmids with a 1 when searching for #1
-		search_params[key]= '\A' + search_params[key] + '\Z'
-
-		Regexp.new(search_params[key], true) #always case insensitive since there's no way to specify
-	end
-
-	def parse_search_regex(search_param)
-
-		matched = REGEX_DETECTION_REGEX.match(search_param)
-		end_of_regex_offset = 1
-
-		case_insensitive = (matched[1].length > 0)
-
-		if case_insensitive then
-			end_of_regex_offset = 2
-		end
-
-		Regexp.new(search_param[1...(search_param.length-end_of_regex_offset)], case_insensitive)
-
-	end
-
-	def generate_regex_conditions(search_params)
-
-		regex_conditions = {}
-		
-		search_params.each_key do |k|
-
-			if search_params[k] and search_params[k] != "" and k != "verified" then #TODO: is there a better way to deal with the verified field?
-
-				matched = REGEX_DETECTION_REGEX.match(search_params[k])
-
-				if matched then
-					regex_conditions[k] = parse_search_regex(search_params[k])
-				else
-					regex_conditions[k] = glob_style_search_to_regex(search_params, k)
-				end
-
-			end
-		end
-
-		regex_conditions
-
-	end
-	
-	def process_search_query(search_params, search_class)
-
-		preprocessed_conditions = preprocess_search_query(search_params)
-
-		regex_conditions = generate_regex_conditions(search_params)
-
-		preprocessed_conditions.each do |k,v|
-			regex_conditions[k] = Regexp.new(v)
-		end
-
-		preliminary_list = search_class.order(index_order)
-
-		final_list = preliminary_list.select do |p|
-			regex_conditions.any? do |k, r|
+    final_list = preliminary_list.select do |p|
+      regex_conditions.any? do |k, r|
         if p.respond_to? k then
-				  val = p.send(k.to_s).to_s
-				  r.match(val)
+          val = p.send(k.to_s).to_s
+          r.match(val)
         else
           nil
         end
-			end
-		end
+      end
+    end
 
-		generate_search_object(search_params, final_list)
+    generate_search_object(search_params, final_list)
+    final_list
+  end
 
-		final_list
-
-	end
-
-	def preprocess_search_query(search_params)
-		{}
-	end
-
-
+  def preprocess_search_query(search_params)
+    {}
+  end
 end
