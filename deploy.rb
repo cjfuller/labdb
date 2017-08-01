2f694840195ef4cfac4359b8337ad71e6ab14685
Colin Fuller <cjfuller@gmail.com>
2017-08-01 12:11:00 -0400

    Don't reset the secret token every deploy; don't change the js version util that js exists.

--- deploy.rb
+++ deploy.rb
@@ -18,14 +18,14 @@ def gcs_path(version)
 end
 
 def before_deploy(version, lab)
-  cmd ["heroku", "config:set", "SECRET_TOKEN=$(rake secret)", "-a labdb-#{lab}"]
-  cmd ["heroku", "config:set", "JS_VERSION=#{version}", "-a labdb-#{lab}"]
+  # cmd ["heroku", "config:set", "SECRET_TOKEN=$(rake secret)", "-a labdb-#{lab}"]
   cmd ['rm', 'public/_s/*.js'], ignore_err: true
   cmd %w(npm install)
   cmd %w(npm run-script coffee-compile)
   cmd %w(npm run-script compile)
   cmd ['gsutil', 'cp', 'public/_s/app_.js', gcs_path(version)]
   cmd ['gsutil', 'acl', 'ch', '-u', 'AllUsers:R', gcs_path(version)]
+  cmd ["heroku", "config:set", "JS_VERSION=#{version}", "-a labdb-#{lab}"]
   puts '-> OK'.green
 end
 
